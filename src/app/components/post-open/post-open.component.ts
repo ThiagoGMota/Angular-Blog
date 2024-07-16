c8216d390040ed122023f10549768af93da42327
Thiago_GMota <thiagogabrielmta@outlook.com.br>
2024-07-16 16:03:52 -0300

    atualização

--- src/app/components/post-open/post-open.component.ts
+++ src/app/components/post-open/post-open.component.ts
@@ -1,15 +1,40 @@
 import { Component, Input } from '@angular/core';
 import { PostCardComponent } from '../post-card/post-card.component';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
-
+import { dataFake } from '../../fake-data/fake-data.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-post-open',
   standalone: true,
-  imports: [PostCardComponent,NavBarComponent],
+  imports: [RouterModule,CommonModule,PostCardComponent,NavBarComponent],
   templateUrl: './post-open.component.html',
   styleUrl: './post-open.component.scss'
 })
 export class PostOpenComponent {
+  /* @Input()
+  titlePost:string =''
+  @Input()
+  datePost:string =''
+  @Input()
+  contentPost:string = ``
+  @Input()
+  id:string = "0"; */
+  @Input()
+  id:string = "0";
+  post = dataFake[0]
+  updatePost(id:string){
+    this.post = dataFake.find(p => p.id == id) || dataFake[0]
+  }
+  dataFake = dataFake;
+  currentPostIndex = 0;
 
+  get currentPost() {
+    return this.dataFake[this.currentPostIndex];
+  }
+
+  showNextPost() {
+    this.currentPostIndex = (this.currentPostIndex + 1) % this.dataFake.length;
+  }
 }
--- src/app/fake-data/fake-data.service.ts
+++ src/app/fake-data/fake-data.service.ts
@@ -17,4 +17,40 @@ export const dataFake = [
     "text":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Lorem",
     "prevPost":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias"
   },
+  {
+    "id":"3",
+    "title":"risrt Post",
+    "date":"14/julho/2024",
+    "cover":"#",
+    "subtitle":"Sub Titulo",
+    "text":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias",
+    "prevPost":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias"
+  },
+  {
+    "id":"4",
+    "title":"Seafacond Post",
+    "date":"15/julho/2024",
+    "cover":"#",
+    "subtitle":"Sub Titulo",
+    "text":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Lorem",
+    "prevPost":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias"
+  },
+  {
+    "id":"5",
+    "title":"Fiafdaasrt Post",
+    "date":"14/julho/2024",
+    "cover":"#",
+    "subtitle":"Sub Titulo",
+    "text":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias",
+    "prevPost":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias"
+  },
+  {
+    "id":"6",
+    "title":"e12Second Post",
+    "date":"125/julho/2024",
+    "cover":"#",
+    "subtitle":"Sub Titulo",
+    "text":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Loremorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias. Lorem",
+    "prevPost":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliasLorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias"
+  },
 ]
